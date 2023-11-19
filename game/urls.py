e472254745b01d0b453383b1a1a5d97922fa4d43
Atarian-ByVoid <cgomespimentel666@gmail.com>
2023-11-19 12:32:28 -0300

    feat: Função de registro e login implementada

--- game/urls.py
+++ game/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import home,about,tutorial
+from .views import home,about, register,tutorial,user_login
 
 urlpatterns = [
     path('', home, name='home'),
     path('about', about, name='about'),
     path('tutorial', tutorial, name='tutorial'),
+    path('login', user_login, name='login'),
+    path('register', register, name='register'),
+
 
 
 ]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+from game.forms import RegisterForm
 
 def home(request):
     return render(request, 'home.html')
@@ -10,4 +14,32 @@ def about(request):
 
 def tutorial(request):
     return render(request, 'tutorial.html')
+
+
+def register(request):
+    if request.POST:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            request.session['message'] = "Registro bem-sucedido!"
+            return redirect('/')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
+
+def user_login(request):
+    user = None
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    return render(request, 'login.html')
 
